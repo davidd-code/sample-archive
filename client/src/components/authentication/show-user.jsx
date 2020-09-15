ca4a864291295cea54bca2a33012dcc305182186
davidd11 <ddoan.0411@gmail.com>
2020-09-15 15:46:25 +1000

    display user details after login

--- client/src/components/authentication/show-user.jsx
+++ client/src/components/authentication/show-user.jsx
@@ -6,7 +6,13 @@ class ShowUserPage extends Component {
         super(props);
         this.state = {
             access_token: this.props.location.pathname.split('=')[1].split('&')[0],
-            refresh_token: this.props.location.pathname.split('=')[2]
+            refresh_token: this.props.location.pathname.split('=')[2],
+            display_name: '',
+            email: '',
+            spotify_uri: '',
+            country: '',
+            profile_img: '',
+            user_id: ''
         }
 
         localStorage.setItem('access_token', this.state.access_token);
@@ -21,6 +27,14 @@ class ShowUserPage extends Component {
             .then(res => {
                 console.log("response received");
                 console.log(res.data)
+                this.setState({
+                    display_name: res.data.display_name,
+                    email: res.data.email,
+                    spotify_uri: res.data.external_urls.spotify,
+                    country: res.data.country,
+                    user_id: res.data.id,
+                    profile_img : res.data.images[0].url
+                })
             })
 
 
@@ -29,7 +43,12 @@ class ShowUserPage extends Component {
     render() {
         return(
             <div>
-                <h1>Hello</h1>
+                <h1>Logged in as {this.state.display_name}</h1>
+                <p>Id {this.state.user_id}</p>
+                <p>Email {this.state.email}</p>
+                <p>Spotify URI {this.state.spotify_uri}</p>
+                <p>Country {this.state.country}</p>
+                <img src={this.state.profile_img}/>
                 <a href="/">Back to home</a>
             </div>
  
